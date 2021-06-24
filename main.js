a24a676044eb4f69f2cc018186282e454399c368
nikolagp <petrovskinikolag@gmail.com>
2021-06-24 18:24:50 +0200

    events added

--- main.js
+++ main.js
@@ -1,1 +1,34 @@
 import './style.css'
+
+//initial count
+let count = 0;
+
+//value and buttons
+const btnDecrease = document.getElementById('btn-decrease');
+const btnIncrease = document.getElementById('btn-increase');
+const btnReset = document.getElementById('btn-reset');
+let value = document.getElementById('value');
+
+let updateValue = function () {
+  value.textContent = count;
+};
+
+btnDecrease.addEventListener("click", function() {
+  value.textContent = count --;
+});
+
+btnIncrease.addEventListener("click", function() {
+  value.textContent = count ++;
+});
+
+btnReset.addEventListener("click", function() {
+  count = 0;
+  value.textContent = count;
+});
+
+
+/*if (value < 0) {
+  document.value.style.color = "red";
+} else {
+  document.value.style.color = "blue";
+}*/
