df5f54ce64e4ebb5c5997ef35667c79ee0a3a0ae
nikolagp <petrovskinikolag@gmail.com>
2021-06-28 12:27:23 +0200

    colors updated - js/css

--- main.js
+++ main.js
@@ -8,27 +8,51 @@ const btnDecrease = document.getElementById('btn-decrease');
 const btnIncrease = document.getElementById('btn-increase');
 const btnReset = document.getElementById('btn-reset');
 let value = document.getElementById('value');
+const valueBox = document.getElementsByClassName("value-box");
 
+//update value function
 let updateValue = function () {
   value.textContent = count;
 };
 
+//update color
+let changeColor = () => {
+  if (count < 0) {
+    value.style.color = "rgb(255, 0, 0)";
+  } else if (count > 0) {
+    value.style.color = "rgb(60, 179, 113)";
+  } else {
+    value.style.color = "rgb(0, 0, 0)"
+  }
+};
+
+//update background
+let borderColor = () => {
+  if (count < 0) {
+    value.style.borderColor = "rgb(255, 0, 0)";
+  } else if (count > 0) {
+    value.style.borderColor = "rgb(60, 179, 113)";
+  } else {
+    value.style.borderColor = "rgb(0, 0, 0)"
+  }
+};
+
+//counter btns
 btnDecrease.addEventListener("click", function() {
   value.textContent = count --;
+  changeColor(); 
+  borderColor();
 });
 
 btnIncrease.addEventListener("click", function() {
   value.textContent = count ++;
+  changeColor();
+  borderColor();
 });
 
 btnReset.addEventListener("click", function() {
   count = 0;
   value.textContent = count;
+  changeColor();
+  borderColor();
 });
-
-
-/*if (value < 0) {
-  document.value.style.color = "red";
-} else {
-  document.value.style.color = "blue";
-}*/
